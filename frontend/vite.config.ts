82474814ec60b6dabc0167a33eef47b05cab3f1f
lexa <lexagiovaniparwata@gmail.com>
2026-08-24 23:43:03 +0800

    chore(frontend): use vitest/config in vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config' // <-- ganti dari 'vite' ke 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
